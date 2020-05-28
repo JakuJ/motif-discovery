0c562259e212a2779e15edf564c0e431c9ee05a5
JakuJ <janaszkiewicz.jakub@gmail.com>
2020-05-28 12:29:00 +0200

    Naprawiono wyciek pamięci

--- src/ensemble.cpp
+++ src/ensemble.cpp
@@ -1,61 +1,60 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <iostream>
+#include <memory>
 #include <string>
 
 #include "../FASCIA/load_graph.hpp"
-#include "../FASCIA/graph.hpp"
 
 int main(int argc, char **argv) {
     // process command line args
     if (argc < 3) {
         std::cout << "Usage: ensemble <path to network> <how many to generate>" << std::endl;
+        return 1;
     }
 
     char *input_path = argv[1];
     int num_graphs = atoi(argv[2]);
-    const int Q = 100;
 
     Graph g;
-    int *sources;
-    int *destinations;
-    int *labels;
-
+    int *sources = nullptr, *destinations = nullptr, *labels = nullptr;
     read_in_graph(g, input_path, false, sources, destinations, labels);
 
-    int edges = g.num_edges() / 2;
+    const int edges = g.num_edges() / 2;
 
     // create output directory
-    std::string path = "./random_graphs";
+    const std::string path {"./random_graphs"};
 
     if (opendir(path.c_str())) {
         system(("rm -rf " + path).c_str());
     }
 
-    const int dir_err = mkdir("./random_graphs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-    if (-1 == dir_err) {
+    if (-1 == mkdir("./random_graphs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)) {
         std::cout << "Error creating directory!" << std::endl;
         exit(1);
     }
+
     // Initialize target arrays
-    int *target_srcs = new int[edges];
-    int *target_dsts = new int[edges];
+    std::unique_ptr<int[]> target_srcs(new int[edges]);
+    std::unique_ptr<int[]> target_dsts(new int[edges]);
 
     // Initialize RNG seeds
-    unsigned int seed1 = time(nullptr);
-    unsigned int seed2 = seed1 + 1;
+    unsigned seed1 = time(nullptr);
+    unsigned seed2 = seed1 + 1;
 
-    for (int iter = 1; iter <= num_graphs; ++iter) {
-        std::cout << "\rGenerating: " << iter << " / " << num_graphs << std::flush;
+    for (auto iter = 1; iter <= num_graphs; ++iter) {
+        if (iter % 10 == 0) {
+            std::cout << "\rGenerating: " << iter << " / " << num_graphs << std::flush;
+        }
 
         // Create a new copy of the initial graph
-        std::copy(sources, sources + edges, target_srcs);
-        std::copy(destinations, destinations + edges, target_dsts);
+        std::copy(sources, sources + edges, target_srcs.get());
+        std::copy(destinations, destinations + edges, target_dsts.get());
 
         // randomly permute edges
-        for (int i = 0; i < Q * edges; ++i) {
-            int e1 = rand_r(&seed1) % edges;
-            int e2 = rand_r(&seed2) % edges;
+        for (int i = 0; i < 100 * edges; ++i) {
+            const int e1 = rand_r(&seed1) % edges;
+            const int e2 = rand_r(&seed2) % edges;
 
             // a -> b, c -> d => a -> d, c -> b
             std::swap(target_dsts[e1], target_dsts[e2]);
@@ -66,7 +65,6 @@ int main(int argc, char **argv) {
         snprintf(filename, sizeof(filename), "./random_graphs/random_%d.graph", iter);
 
         std::ofstream file(filename);
-
         file << g.num_vertices() << std::endl;
         file << edges << std::endl;
 
